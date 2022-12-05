ef45a132c6d75d23b7f34dd1838c2cc3e1686624
wisehero10 <godjw96@gmail.com>
2022-12-05 19:49:38 +0900

    item9 - trywithresources

---
+++ src/com/effective/chap2/item9/tryfinally/TopLine.java
@@ -0,0 +1,21 @@
+package com.effective.chap2.item9.tryfinally;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TopLine {
+	static String firstLineOfFile(String path) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		try {
+			return br.readLine();
+		} finally {
+			br.close();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		String path = args[0];
+		System.out.println(firstLineOfFile(path));
+	}
+}
